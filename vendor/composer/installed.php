78626595cd68956127057ce3feacb322e9fc3e43
Walddda <samuel.g.anmeldung@gmail.com>
2021-04-26 21:53:58 +0200

    vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,60 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '307e4fe5868a2a6352b8de9bb6498958a034985d',
+    'name' => 'quen2/viewalv',
+  ),
+  'versions' => 
+  array (
+    'quen2/viewalv' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '307e4fe5868a2a6352b8de9bb6498958a034985d',
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'v1.22.1',
+      'version' => '1.22.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c6c942b1ac76c82448322025e084cadc56048b4e',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.22.1',
+      'version' => '1.22.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5232de97ee3b75b0360528dae24e73db49566ab1',
+    ),
+    'twig/twig' => 
+    array (
+      'pretty_version' => 'v3.3.0',
+      'version' => '3.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1f3b7e2c06cc05d42936a8ad508ff1db7975cdc5',
+    ),
+    'typo3fluid/fluid' => 
+    array (
+      'pretty_version' => '2.6.10',
+      'version' => '2.6.10.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f20db4e74cf9803c6cffca2ed2f03e1b0b89d0dc',
+    ),
+  ),
+);
